afb18faf2d478a8122ef89f430927ec936c33994
신희성 <vvwv3092@gmail.com>
2023-12-26 22:46:59 +0900

    Updatee cors

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const connection = mysql.createConnection({
   host: "127.0.0.1",
